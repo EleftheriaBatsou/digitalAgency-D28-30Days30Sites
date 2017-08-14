776d3a766c9ec06ada340578febdd79ecf4bc0db
Eleftheria <eleftheriabatsou@gmail.com>
2017-08-14 20:00:32 +0300

    day 28

---
+++ script.js
@@ -0,0 +1,31 @@
+// Find the library for the animation here: https://github.com/jlmakes/scrollreveal?utm_source=newsletter&utm_medium=email&utm_campaign=30_days_30_sites_day_28&utm_term=2017-07-05
+
+
+//window.sr = ScrollReveal();
+// Changing the defaults
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.header',{easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)', duration:'100'});
+
+sr.reveal('.foo', { origin: 'left', scale: 0.9 });
+
+sr.reveal('.bar', { origin: 'right', scale: 0.9});
+
+sr.reveal('.building', { origin: 'top'});
+//sr.reveal('.fourClasses');
+sr.reveal('.one', {delay: 0});
+sr.reveal('.two', {delay: 100});
+sr.reveal('.three', {delay: 250});
+sr.reveal('.four', {delay: 400});
+sr.reveal('.five', {delay: 560});
+sr.reveal('.six', {delay: 700});
+sr.reveal('.seven', {delay: 850});
+sr.reveal('.eight', {delay: 1000});
+
+//sr.reveal('.final');
+sr.reveal('.final-1' , {duration:100, delay: 500});
+sr.reveal('.final-2', {duration:500, delay: 1000});
+
+sr.reveal('.prefooter', { duration: 3000 }, 3);
+
+sr.reveal('#footer', {scale: 1.5, delay: 300});
